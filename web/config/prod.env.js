9608e1c6c6e32e5d5742da06e68571945dbd41a7
chnzrb <chn.zrb@gmail.com>
2018-02-27 09:56:28 +0800

    移除web

--- web/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"http://127.0.0.1:8080"',
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-}
--- web/install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rd /s/q E:\WEB\0226\myadmin\views
-md E:\WEB\0226\myadmin\views
-xcopy /s /e /y  dist  E:\WEB\0226\myadmin\views\
-pause
